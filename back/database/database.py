d55586879d1bf687514d2b72337f1f2504f92e64
Janpol Huinya <huinyajanpol@gmail.com>
2026-04-07 09:31:22 +0300

    init

---
+++ back/database/database.py
@@ -0,0 +1,59 @@
+import peewee
+from peewee import Model, CharField, MySQLDatabase, AutoField, ForeignKeyField
+import pymysql
+
+
+user = "root"
+password = "root"
+host = "localhost"
+port = 3306
+db_name = ""
+
+connection = pymysql.connect(
+    user=user,
+    password=password,
+    database=db_name)
+
+db = MySQLDatabase(
+    db_name,
+    password=password,
+    user=user,
+    host=host,
+    port=port)
+
+
+class Table(Model):
+    id = AutoField()
+
+    class Meta:
+        database = db
+
+
+class Role(Table):
+    name = CharField()
+
+
+class User(Table):
+    name = CharField()
+    role = ForeignKeyField(Role)
+    password = CharField()
+
+
+def test_data():
+    _ = Role.get_or_create(
+        name="admin"
+    )
+
+    _ = User.get_or_create(
+        name="User1",
+        role=1
+    )
+
+
+if __name__ == "__main__":
+    db.create_tables([
+        Role,
+        User
+    ])
+
+    test_data()
